6d27f09815c69b9b4bd0add2ae934f227301aab8
Joseph Gonzalez <josegonzalez2330@gmail.com>
2021-05-03 17:28:42 -0600

    Add CEThread create thread

---
+++ CEThread.h
@@ -0,0 +1,27 @@
+//
+// Created by jglez2330 on 2/5/21.
+//
+#include <ucontext.h>
+#ifndef PROYECTO2OS_CETHREAD_H
+#define PROYECTO2OS_CETHREAD_H
+typedef unsigned long int CEThread_t;
+
+typedef struct {
+    int tid;
+} CEThread_attr_t;
+
+enum State {READY = 0, RUNNING = 1, BLOCKED = 2, TERMINATED = 3};
+typedef struct Thread_t{
+    CEThread_t tid;
+    CEThread_t joining;
+    int state;
+    void* (*pFunction)(void*);
+    void* arg;
+    void* retval;
+    ucontext_t* thread_context;
+} CEThread_treadInfo;
+
+int CEThread_create(CEThread_t* thread, CEThread_attr_t *attr, void* rutine, void* arg);
+
+void CEThread_start(void* (*start_routine)(void*), void* args);
+#endif //PROYECTO2OS_CETHREAD_H
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <sched.h>
+#include <pthread.h>
+int main() {
+    pthread_t t;
+    printf("Hello, World!\n");
+    return 0;
+}

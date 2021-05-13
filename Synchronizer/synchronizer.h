78c827c0b0b399722f73933712c25d96276d29f8
leahycarlos21 <leahyvasquez@gmail.com>
2021-05-13 17:56:06 -0600

    Add Synchronizer init of all methods

---
+++ Synchronizer/synchronizer.h
@@ -0,0 +1,31 @@
+//
+// Created by leahycarlos21 on 13/5/21.
+//
+
+#ifndef PROYECTO2OS_SYNCHRONIZER_H
+#define PROYECTO2OS_SYNCHRONIZER_H
+
+#include "../CEThread.h"
+#include "confuse.h"
+#define channelCount 3
+
+CEThread_mutex_t mutexes[channelCount];
+
+typedef struct pConfig {
+    long calendarizador;
+    long metodoControlFlujo;
+    long largoCanal;
+    long cantidadHormigasOrdenadas;
+    long timeCambioLetrero;
+    long parametroW;
+    long tiempoMaximoRMS;
+}pConfig;
+
+pConfig * valuesChannel;
+
+
+int synchronizerInit();
+int equidad_Init(scheduler_t * scheduler);
+int letrero_Init(scheduler_t * scheduler);
+int tico_Init(scheduler_t * scheduler);
+#endif //PROYECTO2OS_SYNCHRONIZER_H
